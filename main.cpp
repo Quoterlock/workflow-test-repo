16c682ba21f7111964fbec841dfb123d55abc3ab
Quoterlock <vladkrp2003@gmail.com>
2024-07-04 15:23:37 +0300

    new feature added

--- main.cpp
+++ main.cpp
@@ -6,8 +6,13 @@ void feature1() {
     // some text here
 }
 
+void newFeature() {
+    std::cout << "New feature implemented...\n";
+}
+
 int main() {
     std::cout << "Hello world\n";
     feature1();
+    newFeaturse();
     return 0;
 }
